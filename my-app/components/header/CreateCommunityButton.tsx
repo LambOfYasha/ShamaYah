ce1d580055ff2a78ba737de97bab77ab8fa7d81f
LambOfYasha <lambofyashasoftwares@protonmail.com>
2025-07-14 12:49:33 +0300

    Update CreateCommunityButton.tsx

--- my-app/components/header/CreateCommunityButton.tsx
+++ my-app/components/header/CreateCommunityButton.tsx
@@ -8,19 +8,40 @@ import {
     DialogTrigger,
   } from "@/components/ui/dialog"
 
-import React from 'react'
+import React, { useState } from 'react'
+import { useUser } from '@clerk/nextjs' 
+import { Plus } from "lucide-react"
 
 function CreateCommunityButton() {
+
+  const { user } = useUser()
+  const [open, setOpen] = useState(false)
+  const [errorMessage, setErrorMessage] = useState(false)
+
+
+
   return (
-<Dialog>
-  <DialogTrigger>Open</DialogTrigger>
+
+<Dialog open={open} onOpenChange={setOpen}>
+  <DialogTrigger className="w-full p-2 pl-5 flex items-center rounded-md 
+  cursor-pointer bg-black text-white 
+  hover:bg-black transition-all duration-200 
+  disabled:text-sm disabled:opacity-50 disabled:cursor-not-allowed" disabled={!user}>
+    <Plus />
+  {user ? "Create a Community" : "Sign in to create community"}
+  </DialogTrigger>
   <DialogContent>
     <DialogHeader>
-      <DialogTitle>Are you absolutely sure?</DialogTitle>
+      <DialogTitle>Create a Community</DialogTitle>
       <DialogDescription>
-        This action cannot be undone. This will permanently delete your account
-        and remove your data from our servers.
+        Create a community to share your ideas and get feedback from others.
       </DialogDescription>
+
+      <form>
+        {errorMessage && (
+          <div className="text-red-500 text-sm">{errorMessage}</div>
+        )}
+      </form>
     </DialogHeader>
   </DialogContent>
 </Dialog>  )
